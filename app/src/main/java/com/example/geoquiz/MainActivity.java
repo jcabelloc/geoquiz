f9470836e5b3bee88f300e19b2239881fad357e8
jcabelloc <jcabelloc@gmail.com>
2019-05-31 08:59:10 -0500

    # Initial Commit

---
+++ app/src/main/java/com/example/geoquiz/MainActivity.java
@@ -0,0 +1,83 @@
+package com.example.geoquiz;
+
+import android.support.annotation.MainThread;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
+    private Button mNextButton;
+    private TextView mQuestionTextView;
+
+    private Question[] mQuestionBank = new Question[] {
+            new Question(R.string.question_peru, true),
+            new Question(R.string.question_bolivia, false),
+            new Question(R.string.question_ecuador, false)
+    };
+
+    private int mCurrentIndex = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mQuestionTextView = findViewById(R.id.question_text_view);
+        updateQuestion();
+
+        Button mTrueButton = findViewById(R.id.true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAnswer(true);
+            }
+        });
+
+        Button mFalseButton = findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Show Toast on the TOP and Center
+                // Toast toast = Toast.makeText(MainActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT);
+                // toast.setGravity(Gravity.TOP|Gravity.CENTER, 0, 0);
+                // toast.show();
+                checkAnswer(false);
+            }
+        });
+
+        mNextButton = findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                updateQuestion();
+            }
+        });
+    }
+
+    private void updateQuestion() {
+        int question = mQuestionBank[mCurrentIndex].getTextRestId();
+        mQuestionTextView.setText(question);
+    }
+
+    private void checkAnswer(boolean userPressedTrue){
+        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+
+        int messageRestId = 0;
+        if (userPressedTrue == answerIsTrue) {
+            messageRestId = R.string.correct_toast;
+        } else {
+            messageRestId = R.string.incorrect_toast;
+        }
+
+        Toast.makeText(this, messageRestId, Toast.LENGTH_SHORT).show();
+
+    }
+}
---
+++ app/src/main/java/com/example/geoquiz/Question.java
@@ -0,0 +1,28 @@
+package com.example.geoquiz;
+
+public class Question {
+
+    private int mTextRestId;
+    private boolean mAnswerTrue;
+
+    public Question(int textRestId, boolean answerTrue){
+        mTextRestId = textRestId;
+        mAnswerTrue = answerTrue;
+    }
+
+    public int getTextRestId() {
+        return mTextRestId;
+    }
+
+    public void setTextRestId(int textRestId) {
+        mTextRestId = textRestId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}
